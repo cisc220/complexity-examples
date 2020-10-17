160f9ec9d96364a85d300e1204199fdbab6dbf7a
j-b-m-1-0 <66123433+j-b-m-1-0@users.noreply.github.com>
2020-10-17 11:45:48 +0530

    Update linear.cpp

--- linear.cpp
+++ linear.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
-
+using namespace std;
 int main(){
     int number_of_rounds = 0;
-    std::cin >> number_of_rounds;
+    cin >> number_of_rounds;  //by using namespace std you don't have to write std:: scope
     int j = 0;
     for(int i = 0; i < number_of_rounds; i++){
         j = i;
